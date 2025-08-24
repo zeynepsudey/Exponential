8d01f8fc74c245ad2de89b23c12462928a4ba1ba
Zeynep Sude Yücesoy <zeynepsudey00@gmail.com>
2025-08-24 14:16:59 +0300

    last commit

---
+++ src/usluSayi.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class usluSayi {
+
+    public static void main(String[] args) {
+        int n, k;
+
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Üssü alınacak sayı : ");
+        n = inp.nextInt();
+
+        System.out.print("Üs olacak sayı : ");
+        k = inp.nextInt();
+
+        int total = 1;
+
+        for(int i=1; i<=k; i++){
+            total *= n;
+        }
+        System.out.println("Cevap : " + total);
+    }
+}
